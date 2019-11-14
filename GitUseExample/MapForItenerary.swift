7071bdebbf2859d0b062aecf9dd35f4a02daa88c
macmini_home computer <macmini@macmini-homes-mac-mini.local>
2019-11-14 14:42:57 +0530

    Add new map-for-itenerary function

---
+++ GitUseExample/MapForItenerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItenerary.swift
+//  GitUseExample
+//
+//  Created by Home Computer on 14/11/2019.
+//  Copyright © 2019 Home Computer. All rights reserved.
+//
+
+import UIKit
+
+class MapForItenerary: NSObject {
+
+}
